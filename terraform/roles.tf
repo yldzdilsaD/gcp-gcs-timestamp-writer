2c5efc3aedb888efbdb9dbb8c0100bad3df29d00
dyildiz <dilsad.yildiz@adesso.com.tr>
2026-01-06 22:07:04 +0300

    refactor terraform files

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -10,3 +10,8 @@ resource "google_project_iam_member" "cloudrun_roles" {
   role    = each.value
   member  = "serviceAccount:${google_service_account.cloudrun_sa.email}"
 }
+resource "google_project_iam_member" "github_ci_artifact_registry" {
+  project = var.project_id
+  role    = "roles/artifactregistry.writer"
+  member  = "serviceAccount:${google_service_account.github_ci.email}"
+}
