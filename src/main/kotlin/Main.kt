1c37c405826773db0a99733ac7341309ad429889
dyildiz <dilsad.yildiz@adesso.com.tr>
2026-01-06 16:14:39 +0300

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,6 @@ package com.gcp
 
 import com.demo.gcs.GcsWriter
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val bucketName = System.getenv("BUCKET_NAME")
         ?: error("BUCKET_NAME env var is not set")
